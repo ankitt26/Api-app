d521859070ba23af2702016886eb1eb7ded904ba
Ankit <ankit26kushwa@gmail.com>
2023-09-17 21:13:10 +0530

    update schema

---
+++ db/seeds.rb
@@ -0,0 +1,162 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+cars = Car.create([
+    {
+      "name": "Mazda CX-5",
+      "brand": "Mazda",
+      "price": "290,000$"
+    },
+    {
+      "name": "Subaru Outback",
+      "brand": "Subaru",
+      "price": "310,000$"
+    },
+    {
+      "name": "Lexus RX",
+      "brand": "Lexus",
+      "price": "450,000$"
+    },
+    {
+      "name": "Acura MDX",
+      "brand": "Acura",
+      "price": "380,000$"
+    },
+    {
+      "name": "Jeep Wrangler",
+      "brand": "Jeep",
+      "price": "340,000$"
+    },
+    {
+      "name": "Tesla Model 3",
+      "brand": "Tesla",
+      "price": "550,000$"
+    },
+    {
+      "name": "Volvo XC60",
+      "brand": "Volvo",
+      "price": "360,000$"
+    },
+    {
+      "name": "Land Rover Range Rover",
+      "brand": "Land Rover",
+      "price": "600,000$"
+    },
+    {
+      "name": "Kia Sorento",
+      "brand": "Kia",
+      "price": "270,000$"
+    },
+    {
+      "name": "Jaguar F-PACE",
+      "brand": "Jaguar",
+      "price": "490,000$"
+    },
+    {
+      "name": "Hyundai Tucson",
+      "brand": "Hyundai",
+      "price": "260,000$"
+    },
+    {
+      "name": "Ford Escape",
+      "brand": "Ford",
+      "price": "330,000$"
+    },
+    {
+      "name": "Mercedes-Benz GLC",
+      "brand": "Mercedes-Benz",
+      "price": "430,000$"
+    },
+    {
+      "name": "Audi Q5",
+      "brand": "Audi",
+      "price": "390,000$"
+    },
+    {
+      "name": "Nissan Rogue",
+      "brand": "Nissan",
+      "price": "280,000$"
+    },
+    {
+      "name": "Toyota RAV4",
+      "brand": "Toyota",
+      "price": "320,000$"
+    },
+    {
+      "name": "Chevrolet Equinox",
+      "brand": "Chevrolet",
+      "price": "300,000$"
+    },
+    {
+      "name": "Honda CR-V",
+      "brand": "Honda",
+      "price": "310,000$"
+    },
+    {
+      "name": "BMW X5",
+      "brand": "BMW",
+      "price": "480,000$"
+    },
+    {
+      "name": "Volkswagen Tiguan",
+      "brand": "Volkswagen",
+      "price": "330,000$"
+    },
+     {
+          "name": "Toyota Corolla",
+          "brand": "Toyota",
+          "price": "280,000$"
+        },
+        {
+          "name": "Ford Mustang",
+          "brand": "Ford",
+          "price": "350,000$"
+        },
+        {
+          "name": "Chevrolet Silverado",
+          "brand": "Chevrolet",
+          "price": "320,000$"
+        },
+        {
+          "name": "Nissan Altima",
+          "brand": "Nissan",
+          "price": "270,000$"
+        },
+        {
+          "name": "Volkswagen Golf",
+          "brand": "Volkswagen",
+          "price": "290,000$"
+        },
+        {
+          "name": "BMW 3 Series",
+          "brand": "BMW",
+          "price": "400,000$"
+        },
+        {
+          "name": "Mercedes-Benz C-Class",
+          "brand": "Mercedes-Benz",
+          "price": "420,000$"
+        },
+        {
+          "name": "Audi A4",
+          "brand": "Audi",
+          "price": "380,000$"
+        },
+        {
+          "name": "Hyundai Sonata",
+          "brand": "Hyundai",
+          "price": "260,000$"
+        },
+        {
+          "name": "Kia Sportage",
+          "brand": "Kia",
+          "price": "250,000$"
+        }
+      ]
+      
+  )
